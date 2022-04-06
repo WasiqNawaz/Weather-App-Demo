eb7cf1b2883f45746a4938986a0528eb7f03f1ca
Muhammad Wasiq <muhammadwasiq7@gmail.com>
2022-04-07 00:00:19 +0500

    this is demo commit

--- check/ViewController.swift
+++ check/ViewController.swift
@@ -1,9 +1,5 @@
-//
-//  ViewController.swift
-//  check
-//
-//  Created by Muhammad Wasiq on 06/04/2022.
-//
+/********* Developed by Drudots Technology *********/
+/******* https://www.drudotstech.com *********/
 
 import UIKit
 
@@ -15,5 +11,34 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension UILabel {
+    func setTextSpacingBy(value: Double) {
+        if let textString = self.text {
+            let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: value, range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
+}
+
+//Keyboard Handling...
+extension ViewController {
+    func setKeyboardNotificationObserver(){
+        NotificationCenter.default.addObserver(self,selector: #selector(self.keyboardWasShown(notification:)),name: UIResponder.keyboardWillChangeFrameNotification,object: nil)
+        NotificationCenter.default.addObserver(self,selector: #selector(self.keyboardWillHide(sender:)),name: UIResponder.keyboardWillHideNotification,object: nil)
+    }
+    @objc func keyboardWasShown(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        UIView.transition(with: msgTxtView,duration: 1,options: .transitionCrossDissolve,animations: {
+            self.txtViewBottomConstraint.constant = (keyboardFrame.size.height)
+            //            self.bottomMove()
+        },completion: nil)
+    }
+    @objc func keyboardWillHide(sender: NSNotification) {
+        UIView.transition(with: msgTxtView,duration: 0.2,options: .transitionCrossDissolve,animations: {self.txtViewBottomConstraint.constant = 40.0},completion: nil)
+    }
 }
 
