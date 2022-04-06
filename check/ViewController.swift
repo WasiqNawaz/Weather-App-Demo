9c4acf29b76acf6d170fb6a91722a35d84b0c17c
Muhammad Wasiq <muhammadwasiq7@gmail.com>
2022-04-07 02:36:52 +0500

    location modified

--- check/ViewController.swift
+++ check/ViewController.swift
@@ -27,39 +27,18 @@ extension UILabel {
 extension GetLocationVC: CLLocationManagerDelegate{
     func getTheLocation(){
         locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        
+  
         if CLLocationManager.locationServicesEnabled(){
             locationManager.startUpdatingLocation()
         }
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation :CLLocation = locations[0] as CLLocation
-        locationManager.stopUpdatingLocation()
-        print("User latitude = \(userLocation.coordinate.latitude)")
-        print("User longitude = \(userLocation.coordinate.longitude)")
-        
-        let geocoder = CLGeocoder()
-        geocoder.reverseGeocodeLocation(userLocation) { (placemarks, error) in
-            if (error != nil){
-                print("error in reverseGeocode")
-            }
-            let placemark = placemarks! as [CLPlacemark]
-            if placemark.count>0{
-                let placemark = placemarks![0]
-                print(placemark.locality!)
-                print(placemark.administrativeArea!)
-                print(placemark.country!)
-                let myLocation = "\(placemark.locality!), \(placemark.country!)"
-                
-                self.locationTextView.text = myLocation
-                print("Hey there! Here is you location: \(placemark.locality!), \(placemark.administrativeArea!), \(placemark.country!)")
-            }
-        }
-    }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error \(error)")
-    }
-}
+       
+
+//MARK:- Variables
+
+//MARK:- Arrays
+
+//MARK:- Outlets
+
+sdfsdfsdf
